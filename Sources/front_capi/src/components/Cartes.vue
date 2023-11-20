2460300bb15883f9548079f40ef4763d53c7a446
MattGuil <guillemin.matt@gmail.com>
2023-11-20 11:25:42 +0100

    affichage test des cartes

---
+++ Sources/front_capi/src/components/Cartes.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="cartes">
+    <div v-for="(image, index) in images" :key="index">
+      <img :src="image" alt="Image" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartesVue',
+  data() {
+    return {
+      images: [],
+    };
+  },
+  mounted() {
+    this.importImages();
+  },
+  methods: {
+    importImages() {
+      const imagesContext = require.context('@/assets/cartes/', false, /cartes_.*\.svg$/);
+      this.images = imagesContext.keys().map(imagesContext);
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+#cartes {
+  width: 100vw;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+}
+
+#cartes img {
+  width: 100%;
+}
+
+</style>
