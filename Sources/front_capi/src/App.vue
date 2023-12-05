41328a3501dafae5f93c316b537e5b2f1ab6c71d
MattGuil <guillemin.matt@gmail.com>
2023-12-05 16:54:32 +0100

    ajout de la bannière latérale (PLANNING POKER)

--- Sources/front_capi/src/App.vue
+++ Sources/front_capi/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <router-view id="content"></router-view>
+  <p>PLANNING<br>POKER</p>
 </template>
 
 <script>
@@ -29,6 +30,17 @@ export default {
 @font-face {
     font-family: ObjectSans-Slanted;
     src: url('@/assets/fonts/PPObjectSans-Slanted.otf');
+}
+
+p {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  font-family: ObjectSans-Heavy;
+  line-height: .8;
+  font-size: 1.5rem;
+  transform: translate(-15%, -200%) rotate(-90deg);
+  opacity: .5;
 }
 
 </style>
