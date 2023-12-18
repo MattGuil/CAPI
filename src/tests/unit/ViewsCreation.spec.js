89a94d60281c28de246548eedf89391b9e856287
MattGuil <guillemin.matt@gmail.com>
2023-12-18 21:26:05 +0100

    retrait de la vue Results dans le fichier test

--- src/tests/unit/ViewsCreation.spec.js
+++ src/tests/unit/ViewsCreation.spec.js
@@ -4,7 +4,6 @@ import { describe } from 'vitest';
 import Dashboard from "../../views/Dashboard.vue";
 import Cardsboard from "../../views/CardsBoard.vue";
 import CoffeeBreak from "../../views/CoffeeBreak.vue";
-import Results from "../../views/Results.vue";
 
 describe('Dashboard', () => {
     it('est monté avec succès', () => {
@@ -25,14 +24,6 @@ describe('Cardsboard', () => {
 describe('CoffeeBreak', () => {
     it('est monté avec succès', () => {
         const wrapper = mount(CoffeeBreak);
-
-        expect(wrapper.exists()).toBe(true);
-    });
-});
-
-describe('Results', () => {
-    it('est monté avec succès', () => {
-        const wrapper = mount(Results);
 
         expect(wrapper.exists()).toBe(true);
     });
