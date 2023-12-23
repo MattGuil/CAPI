26227c4b3fe9362526d1b7731e8237602127b5b4
MattGuil <guillemin.matt@gmail.com>
2023-12-24 00:52:55 +0100

    simplification des tests à l'extrême

--- src/tests/unit/ViewsCreation.spec.js
+++ src/tests/unit/ViewsCreation.spec.js
@@ -1,25 +1,7 @@
 import { mount } from '@vue/test-utils'
 import { describe } from 'vitest';
 
-import Dashboard from "../../views/Dashboard.vue";
-import Cardsboard from "../../views/CardsBoard.vue";
 import CoffeeBreak from "../../views/CoffeeBreak.vue";
-
-describe('Dashboard', () => {
-    it('est monté avec succès', () => {
-        const wrapper = mount(Dashboard);
-
-        expect(wrapper.exists()).toBe(true);
-    });
-});
-
-describe('Cardsboard', () => {
-    it('est monté avec succès', () => {
-        const wrapper = mount(Cardsboard);
-
-        expect(wrapper.exists()).toBe(true);
-    });
-});
 
 describe('CoffeeBreak', () => {
     it('est monté avec succès', () => {
