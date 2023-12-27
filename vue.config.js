d61d5ef2184590f32d07013cd4a7f5d5c7793131
MattGuil <guillemin.matt@gmail.com>
2023-12-27 11:41:29 +0100

    Create deploy.yml

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
     host: "localhost"
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/CAPI/' : '/',
 }
