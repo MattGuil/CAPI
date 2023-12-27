de590647ff8ece41643ba27514bffe730c683fc4
MattGuil <guillemin.matt@gmail.com>
2023-12-27 14:21:42 +0100

    change components import method in router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import Menu from '../views/Menu.vue';
+import Dashboard from '../views/Dashboard.vue';
+import CardsBoard from '../views/CardsBoard.vue';
+import Chart from '../views/Chart.vue';
+import CoffeeBreak from '../views/CoffeeBreak.vue';
+import Results from '../views/Results.vue';
+
 const routes = [
   {
     path: "/",
     name: "Menu",
-    component: () => import("@/views/Menu.vue"),
+    component: Menu
   },
   {
     path: "/dashboard",
     name: "Dashboard",
-    component: () => import("@/views/Dashboard.vue"),
+    component: Dashboard
   },
   {
     path: "/cardsboard",
     name: "CardsBoard",
-    component: () => import("@/views/CardsBoard.vue"),
+    component: CardsBoard
   },
   {
     path: "/chart",
     name: "Chart",
-    component: () => import("@/views/Chart.vue"),
+    component: Chart
   },
   {
     path: "/coffeebreak",
     name: "CoffeeBreak",
-    component: () => import("@/views/CoffeeBreak.vue"),
+    component: CoffeeBreak
   },
   {
     path: "/results",
     name: "Results",
-    component: () => import("@/views/Results.vue"),
+    component: Results
   },
 ];
 
